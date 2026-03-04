d7a00a0ca58d7c694eaadce59de556c80a38577b
Khemraj9815 <02230286.cst@rub.edu.bt>
2026-03-04 13:10:24 +0600

    wip: practical 1

---
+++ PRACTICAL-00/Q-01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string name = "Khem Raj Ghalley";
+    int std_no;
+    std_no = 2230286;
+    string course = "BE Software Engineering";
+    
+    cout << "==========================\n" << endl;
+    cout << "Student Profile\n";
+    cout << "==========================\n";
+    cout << "Name:  " << name << "\n" << endl;
+    cout << "Student No:  " << std_no << "\n" << endl;
+    cout << "Course:  " << course << "\n" << endl;
+    cout << "==========================\n" << endl;
+
+    return 0;
+}
---
+++ PRACTICAL-00/Q-02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include string
+using namespace std;
+
+int main() {
+    int std_no = 2230286;
+    int sum = 0;
+    int temp = number
+
+    for (;)
+}
